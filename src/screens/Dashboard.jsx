3e21e4bf041bf12cbf1e9a78a5a2d31b6dfd1e4e
Peter Francis C. Robante <robantepeterfran@gmail.com>
2024-03-12 17:14:17 +0800

    feat:dashboard toolbar responsive

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -14,28 +14,28 @@ const Dashboard = () => {
         <Navbar/>
         <div className="relative flex flex-row justify-center items-center w-full h-[100vh] pt-[105px] p-10 bg-[#F7F9F9]">
             <div className="bg-white relative w-full h-full rounded-[10px] shadow-lg border-t-4 p-5 border-[#F5D45E]">
-                <div className="flex flex-row justify-between items-center bg-green-100">
-                    <div className="relative flex flec-row">
+                <div className="flex flex-col gap-4 lg:gap-0 lg:flex-row justify-between items-center">
+                    <div className="relative flex flex-row">
                         <FiSearch className="absolute right-3 top-[7px] text-[20px]"/>
                         <input
                         placeholder="search"
-                        className="p-[3px] pr-[35px] pl-2 border-[#dfdfdf] border-[2px] rounded-[20px] w-[320px]"
+                        className="p-[3px] pr-[35px] pl-4 border-[#dfdfdf] border-[2px] rounded-[20px] w-[100%] md:w-[420px] lg:w-[320px]"
                         />
                     </div>
-                    <div className="flex flex-row gap-2">
-                        <button className="flex flex-row  gap-1 px-2 items-center bg-[#F5D45E] py-[6px] text-white text-[14px] p-[4px] rounded-lg">
+                    <div className="flex flex-wrap-reverse  flex-row gap-2">
+                        <button className="flex flex-row  gap-1 px-2 items-center bg-[#F5D45E] hover:bg-[#f5c35e] py-[6px] text-white text-[14px] p-[4px] rounded-lg">
                             <TbFileImport className="text-[20px]"/>
                             <p className="font-bold">IMPORT EXCEL</p>
                         </button>
-                        <button className="flex flex-row  gap-1 px-2 items-center bg-[#ED6559] text-white text-[14px] p-[4px] rounded-lg">
+                        <button className="flex flex-row  gap-1 px-2 items-center bg-[#ED6559] hover:bg-[#b94242] py-[6px] text-white text-[14px] p-[4px] rounded-lg">
                             <TbArrowsExchange2 className="text-[20px]"/>
                             <p className="font-bold">CHANGE CERTIFICATE</p>
                         </button>
-                        <button className="flex flex-row  gap-1 px-2 items-center bg-[#47A2FF] text-white text-[14px] p-[4px] rounded-lg">
+                        <button className="flex flex-row  gap-1 px-2 items-center bg-[#47A2FF] hover:bg-[#477eff] py-[6px] text-white text-[14px] p-[4px] rounded-lg">
                             <MdAdd className="text-[20px]"/>
                             <p className="font-bold">ADD STUDENT</p>
                         </button>
-                        <button className="flex flex-row  gap-1 px-2 items-center bg-[#5AC648] text-white text-[14px] p-[4px] rounded-lg">
+                        <button className="flex flex-row  gap-1 px-2 items-center bg-[#5AC648] hover:bg-[#4fac3f] py-[6px] text-white text-[14px] p-[4px] rounded-lg">
                             <FiPrinter className="text-[20px]"/>
                             <p className="font-bold">PRINT ALL</p>
                         </button>
